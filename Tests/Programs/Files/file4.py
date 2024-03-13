b217e60506a3a8877e56a14671dbafa4875d143c
= <=>
2024-03-13 15:26:30 +0530

    file4 gets updated

---
+++ Tests/Programs/Files/file4.py
@@ -0,0 +1,14 @@
+# Python program to read character by character from a file
+with open("C:/Users/shashi/Desktop/machine learning.txt") as f:
+    data=f.readlines()
+    for line in data:
+        for words in line.split():
+            for word in words.split():
+                for ch in word.split():
+                    print(ch,end='\n')
+<<<<<<< Updated upstream
+
+
+# new lines added here
+=======
+>>>>>>> Stashed changes
